f6c88358774f70dba4ba53863469473e807ebfdc
Prabhu <prabh.rao@gmail.com>
2023-04-05 13:09:41 +0530

    roboshop docdb jenkins

--- vars.tf
+++ vars.tf
@@ -1,26 +1,19 @@
+variable "folders" {
+  default = ["Infra", "CI-Pipeline"]
+}
 
-#folders = ["Infra"]
-#jobs = [
-#    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/prabhudevops123/roboshop-infra-new" }
-#  ]
+variable "jobs" {
+  default = [
+    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/prabhudevops123/roboshop-infra-new" },
+    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/prabhudevops123/frontend" },
+    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/prabhudevops123/cart" },
+    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/prabhudevops123/catalogue" },
+    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/prabhudevops123/user" },
+    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/prabhudevops123/shipping" },
+    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/prabhudevops123/payment" }
+  ]
+}
 
-
-#variable "folders" {
-#  default = ["Infra", "CI-Pipeline"]
-#}
-#
-#variable "jobs" {
-#  default = [
-#    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/prabhudevops123/roboshop-infra-new" },
-#    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/prabhudevops123/frontend" },
-#    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/prabhudevops123/cart" },
-#    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/prabhudevops123/catalogue" },
-#    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/prabhudevops123/user" },
-#    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/prabhudevops123/shipping" },
-#    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/prabhudevops123/payment" }
-#  ]
-#}
-#
-#variable "force" {
-#  default = false
-#}
+variable "force" {
+  default = false
+}
